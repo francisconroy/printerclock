a7c3b2157ea7b096ed50478fdff3822eb3eea379
francisconroy <francisedwardconroy@gmail.com>
2017-11-22 22:48:59 +0100

    Add http thread

--- runsystem.py
+++ runsystem.py
@@ -19,33 +19,35 @@ class ClockThread(threading.Thread):
             clockstepper.gotoposition(pos)
             time.sleep(30)
 
-## Web server config
-server_address = ('', 80)
+class HTTPThread(threading.Thread):
+    def run(self):
+        ## Web server config
+        server_address = ('', 80)
+        import auth
+        configfile = auth.ConfigFile("userdata.txt")
+        configfile.print_users()
 
-print("Initialising the system...")
-# Init auth
-import auth
-configfile = auth.ConfigFile("userdata.txt")
-configfile.print_users()
+        door_status = ds.DoorStat(np.pin_dict_door_status)
 
-door_status = ds.DoorStat(np.pin_dict_door_status)
+        door_driver = sm.StepperMotorTB6560(np.pin_dict_TB6560, sm.StepperMotorTB6560.type, 0, 260)
+        room_door = do.Door(door_status, door_driver)
 
-door_driver = sm.StepperMotorTB6560(np.pin_dict_TB6560, sm.StepperMotorTB6560.type, 0, 260)
-room_door = do.Door(door_status, door_driver)
-
-handler_class = httpserver.Server
-handler_class.getfunc = door_status.check_door_status
-handler_class.getfunc_args = ()
-handler_class.postfunc = configfile.checkpin_from_dict
-handler_class.postactionfunc = room_door.open_door
-server_class = BaseHTTPServer.HTTPServer
-httpd = server_class(server_address, handler_class)
+        handler_class = httpserver.Server
+        handler_class.getfunc = door_status.check_door_status
+        handler_class.getfunc_args = ()
+        handler_class.postfunc = configfile.checkpin_from_dict
+        handler_class.postactionfunc = room_door.open_door
+        server_class = BaseHTTPServer.HTTPServer
+        httpd = server_class(server_address, handler_class)
+        ## Init web server
+        httpd.serve_forever()
 
 
 ## Start threads
 print("Starting threads...")
 clockthread = ClockThread(name="Clock thread")
 clockthread.start()
+httpthread = HTTPThread(name="HTTP thread")
+httpthread.start()
 
-## Init web server
-httpd.serve_forever()
+
--- test_lines.py
+++ test_lines.py
@@ -6,4 +6,4 @@ import nymph_pins as np
 
 doormotor = sm.StepperMotorTB6560(np.pin_dict_TB6560, sm.StepperMotorTB6560.type, 0, 260)
 
-clockmotor = sm.StepperMotorA4988(np.pin_dict_a4988, sm.StepperMotorA4988.type, 0, 6800)
+clockmotor = sm.StepperMotorA4988(np.pin_dict_a4988, sm.StepperMotorA4988.type, 0, 6800, override=True)
