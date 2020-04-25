5abb7612d185de74dae899297ce1e92bd5fcf379
Francis Conroy <francisc@posteo.net>
2020-04-25 13:46:34 +1000

    Implement more of this function

--- motor_calibration.py
+++ motor_calibration.py
@@ -2,22 +2,29 @@ import stepmotor as sm
 motors = [sm.StepperMotorTB6560(),
           sm.StepperMotorA4988()]
 
-
-
-
-while(1):
+while 1:
     print("Motors:")
     for i in range(len(motors)):
         print(" {} - {}".format(i, motors[i].name))
-    motor_index = raw_input("Please select a motor to calibrate:")
-    print("Please use the a/d keys to position the motor")
-    while(1):
-        command = getch()
-        if command == 'a':
-            # Move CCW
+    motor_index = input("Please select a motor to calibrate:")
+    if motor_index >= len(motors):
+        print("Invalid selection")
+        exit(-1)
+    motor = motors[motor_index]
+    print("Move the motor to the zero position")
+    while 1:
+        command = input("cmd:")
+        if command == 'c':
+            print("saving cal")
+            motor.calibrate()
+        elif command == 'e':
+            print("Exiting!")
+            exit(0)
+        else:
+            try:
+                value = int(command)
+                motor.multistep(value)
+            finally:
+                pass
 
-        if command == 'd':
-            # Move CW
-
-        if command == ''
 
